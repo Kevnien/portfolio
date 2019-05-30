e23a63fb00651d6a883d76d3f89ba4edc15f8f4d
Kevin Nguyen <kevinn858@gmail.com>
2019-05-29 18:56:48 -0700

    added a portfolio item

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,6 +7,26 @@ function App() {
       <header className="App-header">
         Kevin Nguyen's Portfolio Website
       </header>
+      <div className="items">
+        <div className="one">
+          <div className="item-title item-div">
+            ShopTrak
+          </div>
+          <div className="item-desc item-div">
+            Web app where users can share shopping lists with others
+          </div>
+          <div className="item-app item-div link">
+            <a href="https://labs10-shopping-list.netlify.com/">
+              app
+            </a>
+          </div>
+          <div className="item-github item-div link">
+            <a href="https://github.com/Lambda-School-Labs/labs10-shopping-list">
+              github
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
